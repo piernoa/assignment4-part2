8a83962ebca5dbfb5bc585a45ba2a6132def8e31
piernoa <piernoa@onid.oregonstate.edu>
2015-05-08 14:05:04 -0700

    Update connection.php

--- src/connection.php
+++ src/connection.php
@@ -1,6 +1,7 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
+// This is not the actual server connection used for the assignment. I didn't want to post my server credentials here.
 $mysqli = new mysqli("localhost", "root", "root", "school");
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
